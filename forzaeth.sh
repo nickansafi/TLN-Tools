4348d546cd7c11dc7ecd1abc00cec759121fda93
nickansafi <nickansafi@outlook.com>
2025-07-26 21:02:17 -0500

    fix

--- forzaeth.sh
+++ forzaeth.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y ros-noetic-tf2-geometry-msgs ros-noetic-ackermann-msgs ros-noetic-joy ros-noetic-map-server lua5.3-dev
+sudo apt-get install -y ros-noetic-tf2-geometry-msgs ros-noetic-ackermann-msgs ros-noetic-joy ros-noetic-map-server lua5.3-dev ninja-build
 mkdir forzaeth_ws
 cd forzaeth_ws
 mkdir src
